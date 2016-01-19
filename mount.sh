396d86738964578971183de36c739299640c1dcd
u <u@ii.fr>
2016-01-19 11:26:00 +0100

    version 0.2. found error while mounting : patched it

--- mount.sh
+++ mount.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-i=0
-while [ $i -le 1 ]
-do
-   if [ -b /dev/sda1 ]
-	then 
-	break
-
-fi
-done 
-
-mount /dev/sda1
-
 if [ -l /sys/class/gpio/gpio21]
 	then
 		echo "fichier gpio21 existe "
@@ -19,16 +7,42 @@ if [ -l /sys/class/gpio/gpio21]
 		echo 21 > /sys/class/gpio/export
 
 fi
-
-
 echo out > /sys/class/gpio/gpio21/direction
 
-unmount /dev/sda1
-
-for j in 1 2 3 
+i=0
+while [ $i -le 1 ]
 do
 	echo 1 > /sys/class/gpio/gpio21/value
 	sleep 1
 	echo 0 > /sys/class/gpio/gpio21/value
 	sleep 1
+
+
+
+   if [ -b /dev/sda1 ]
+	then 
+	break
+   		
+
+fi
+done 
+if [ -d /media/external ]
+	then
+		echo "media/external existe"
+	else
+		mkdir /media/external/
+
+fi
+
+mount /dev/sda1 /media/external/
+
+
+umount /media/external/
+
+for j in 1 2 3 
+do
+	echo 1 > /sys/class/gpio/gpio21/value
+	usleep 500000
+	echo 0 > /sys/class/gpio/gpio21/value
+	usleep 500000
 done
