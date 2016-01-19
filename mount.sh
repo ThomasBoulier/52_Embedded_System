bd108c3318f57be96518b822cb29102ab5cdedc8
u <u@ii.fr>
2016-01-19 10:16:56 +0100

    Bash script added to mount the usb stick

---
+++ mount.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+i=0
+while [ $i -le 1 ]
+do
+   if [ -b /dev/sda1 ]
+	then	
+	break
+
+fi
+done 
+
+mount /dev/sda1
+
+if [ -l /sys/class/gpio/gpio21]
+	then
+		echo "fichier gpio21 existe "
+	else
+		echo 21 > /sys/class/gpio/export
+
+fi
+
+
+echo out > /sys/class/gpio/gpio21/direction
+echo 1 > /sys/class/gpio/gpio21/value
