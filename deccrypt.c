acadd22aca1b68cd483fff9b31aa7d9aa7fbc56a
Thomas Boulier <ThomasBoulier@users.noreply.github.com>
2016-02-02 12:10:35 +0100

    Create deccrypt.c

---
+++ deccrypt.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <fcntl.h>
+#define TAILLE_MAX 50
+
+
+void clean(const char *buffer, FILE *fp);
+void encryptDecrypt(char *input, char *output);
+
+
+
+int main(int argc, char *argv[]){
+
+	
+	DIR *dir = NULL;
+	struct dirent *envelopDir = NULL;
+
+	dir = opendir("/media/external/");
+	if(dir == NULL){
+		printf("Error. Cannot open /media/external.\n");
+		exit(1);
+	}
+	printf("The directory /media/external has been properly opened.\n\n");
+	
+
+	int fr = open("/media/external/encryptedFile.xor",O_RDONLY);
+	int fw = open ("/media/external/decryptedFile.txt",O_WRONLY | O_CREAT);
+	if (fr<0){
+		printf("Error. Cannot open encryptedFile.xor.\n");
+		exit(1);
+	}
+	if (fw<0){
+		printf("Error. Cannot write decryptedFile.txt \n");
+		exit(1);
+	}
+
+	char buffer_in[TAILLE_MAX]="";
+	char buffer_out[TAILLE_MAX]="";
+	read(fr, buffer_in, TAILLE_MAX);
+	encryptDecrypt(buffer_in, buffer_out);
+
+	printf("%s\n",buffer_out);
+
+        int i = 0;
+        for(i = 0; i < TAILLE_MAX; i++)
+		printf("%02x ", buffer_out[i]);
+	printf("\n");
+
+	int f = write(fw,buffer_out,TAILLE_MAX);
+
+	printf("%i\n",f);
+	close(fr);
+	close(fw);
+	close(dir);
+
+
+	
+	
+	return 0;
+}
+
+
+
+
+void clean(const char *buffer, FILE *fp){
+    char *p = strchr(buffer,'\n');
+
+    if (p != NULL)
+        *p = 0;
+    else{
+        int c;
+
+        while ((c = fgetc(fp)) != '\n' && c != EOF);
+
+    }
+}
+
+
+
+void encryptDecrypt(char *input, char *output){
+	char key[] = {'K','A','D'}; //Can be any chars, and any size array
+	
+	int i;
+	for(i = 0; i < strlen(input); i++) {
+		output[i] = input[i] ^ key[i % (sizeof(key)/sizeof(char))];
+	}
+}
+
+
+
+
